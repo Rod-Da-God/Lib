34b085bc330c279a29ce07c0ea978ddec5e3c986
toxic9time <stepnaerimov@gmail.com>
2021-03-26 20:49:17 +1000

    #2

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -1,129 +1,187 @@
 package com.mygdx.game;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.logging.Level;
 
 import javax.swing.JOptionPane;
 
-public class Game extends ApplicationAdapter {
-	OrthographicCamera camera;
-	SpriteBatch batch;
-	Texture dropImage;
-	Texture bucketImage;
-	Sound dropSound;
-	Music rainMusic;
-	Rectangle bucket;
-	Vector3 touchPos;
-	Array<Rectangle> raindrops;
-	long lastDropTime;
-	public int Score = 0;
+import sun.rmi.runtime.Log;
 
-	@Override
-	public void create () {
+public class Game extends ApplicationAdapter  {
+Game game;
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    Texture dropImage;
+    Texture bucketImage;
+    Sound dropSound;
+    Music rainMusic;
+    Rectangle bucket;
+    Vector3 vector;
+    Array<Rectangle> raindrops;
+    long lastDropTime;
+    static Texture backTexture;
+    static Sprite backSprite;
 
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+    @Override
+    public void create() {
 
-		batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 
-		touchPos = new Vector3();
+        batch = new SpriteBatch();
 
-		dropImage = new Texture("droplet.png");
-		bucketImage = new Texture("f.jpg");
+        vector = new Vector3();
 
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("waterdrop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("undertreeinrain.mp3"));
+        dropImage = new Texture("droplet.png");
+        bucketImage = new Texture("bucket.png");
 
-		rainMusic.setLooping(true);
-		rainMusic.play();
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("waterdrop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("undertreeinrain.mp3"));
 
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
+        rainMusic.setLooping(true);
+        rainMusic.play();
 
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
 
-	}
+        raindrops = new Array<Rectangle>();
+        spawnRaindrop();
 
-	private void spawnRaindrop(){
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800-64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
-	}
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
 
-		camera.update();
 
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        for (Rectangle raindrop : raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        batch.end();
 
-		for (Rectangle raindrop: raindrops){
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
+        if (Gdx.input.isTouched()) {
+            vector.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(vector);
+            bucket.x = (int) (vector.x - 64 / 2);
+            bucket.y = (int) (vector.y - 64 / 2);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+            bucket.y += 10000 * Gdx.graphics.getDeltaTime();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) {
+            bucket.x += 10000 * Gdx.graphics.getDeltaTime();
+        }
 
-		if(Gdx.input.isTouched()){
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			camera.unproject(touchPos);
-			bucket.x = (int) (touchPos.x -64 / 2);
-			bucket.y = (int) (touchPos.y -64 / 2);
-		}
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            bucket.x -= 500 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            bucket.x += 500 * Gdx.graphics.getDeltaTime();
 
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 500 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 500 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y -= 500 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y += 500 * Gdx.graphics.getDeltaTime();
 
-		if (bucket.x < 0) bucket.x = 0;
-		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
-		if (bucket.y < 0) bucket.y = 0;
-		if (bucket.y > 480 - 64) bucket.y = 480 - 64;
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+            bucket.x -= 500 * Gdx.graphics.getDeltaTime();
 
-		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+            bucket.x += 500 * Gdx.graphics.getDeltaTime();
 
-		Iterator<Rectangle> iter = raindrops.iterator();
-		while (iter.hasNext()){
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if (raindrop.y + 64 < 0) iter.remove();
-			if (raindrop.overlaps(bucket)){
-				dropSound.play();
-				iter.remove();
-			}
-		}
-	}
 
-	@Override
-	public void dispose() {
-		super.dispose();
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-		rainMusic.dispose();
-		batch.dispose();
-	}
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) bucket.y -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) bucket.y += 200 * Gdx.graphics.getDeltaTime();
+
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) bucket.y -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) bucket.y += 200 * Gdx.graphics.getDeltaTime();
+
+
+        if (this.bucket.x < 0) bucket.x = 800 - 64;
+        if (bucket.x > 800 - 64) bucket.x = 0;
+
+        if (bucket.y < 0) bucket.y = 480 - 64;
+        if (bucket.y > 480 - 64) bucket.y = 0;
+
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while (iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0) iter.remove();
+            if (raindrop.overlaps(bucket)) {
+                dropSound.play();
+                iter.remove();
+
+
+            }
+        }
+    }
+
+
+    public void show() {
+
+    }
+
+
+
+
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
+        batch.dispose();
+    }
 }
+
+
+
+
+
+
+
---
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -0,0 +1,142 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.logging.Logger;
+
+import sun.rmi.runtime.Log;
+
+public class MenuScreen implements Screen {
+    MainMenuScreen menuScreen;
+
+    Vector3 vector3 = new Vector3();
+
+    Music Cyber;
+    SpriteBatch sBatch;
+    OrthographicCamera oCamera;
+
+    Texture startButton;
+    Texture exitButton;
+    Texture background;
+    Sprite S_startButton;
+    Sprite S_exitButton;
+    Sprite S_background;
+
+    private static float BUTTON_RESIZE_FACTOR = 800f;
+    private static float START_VERT_POSITION_FACTOR = 2.7f;
+    private static float EXIT_VERT_POSITION_FACTOR = 4.2f;
+
+    MenuScreen(MainMenuScreen menuScreen) {
+
+        this.menuScreen = menuScreen;
+
+
+        float heightY = Gdx.graphics.getHeight();
+        float weightX = Gdx.graphics.getWidth();
+
+        oCamera = new OrthographicCamera(heightY, weightX);
+        oCamera.setToOrtho(false);
+        sBatch = new SpriteBatch();
+
+
+        startButton = new Texture("startButton.jpg");
+        exitButton = new Texture("exitButton.jpg");
+        background = new Texture("background.jpg");
+        Cyber =  Gdx.audio.newMusic(Gdx.files.internal("undertreeinrain.mp3"));
+        Cyber.setLooping(true);
+        Cyber.play();
+
+
+        S_startButton = new Sprite(startButton);
+        S_exitButton = new Sprite(exitButton);
+        S_background = new Sprite(background);
+
+
+        S_startButton.setPosition(S_startButton.getWidth() * (weightX / BUTTON_RESIZE_FACTOR), S_startButton.getHeight() * (weightX / BUTTON_RESIZE_FACTOR));
+        S_exitButton.setSize(S_exitButton.getWidth() * (weightX / BUTTON_RESIZE_FACTOR), S_exitButton.getHeight() * (weightX / BUTTON_RESIZE_FACTOR));
+        S_background.setSize(weightX, heightY);
+        S_startButton.setPosition((weightX / 2f - S_startButton.getWidth() / 2), weightX / START_VERT_POSITION_FACTOR);
+        S_exitButton.setPosition((weightX / 2f - S_exitButton.getWidth() / 2), weightX / EXIT_VERT_POSITION_FACTOR);
+        S_background.setAlpha(0.8f);
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sBatch.setProjectionMatrix(oCamera.combined);
+        sBatch.begin();
+        S_background.draw(sBatch);
+        S_startButton.draw(sBatch);
+        S_exitButton.draw(sBatch);
+        handleTouch();
+        sBatch.end();
+    }
+
+
+    void handleTouch(){
+        // Проверяем были ли касание по экрану?
+        if(Gdx.input.justTouched()) {
+            // Получаем координаты касания и устанавливаем эти значения в временный вектор
+            vector3.set(Gdx.input.getX(),Gdx.input.getY(), 0);
+            // получаем координаты касания относительно области просмотра нашей камеры
+            oCamera.unproject(vector3);
+            float touchX = vector3.x;
+            float touchY= vector3.y;
+            // обработка касания по кнопке Stare
+            if((touchX>=S_startButton.getX()) && touchX<= (S_startButton.getX()+S_startButton.getWidth()) && (touchY>=S_startButton.getY()) && touchY<=(S_startButton.getY()+S_startButton.getHeight()) ){
+                Gdx.app.debug("GameScreen", "my error message");
+                System.out.println("sadad");
+                menuScreen.setScreen((Screen) new Game()); // Переход к экрану игры
+                Gdx.app.debug("GameScreen", "error");
+                System.out.println("sadad");
+            }
+            // обработка касания по кнопке Exit
+            else if((touchX>=S_exitButton.getX()) && touchX<= (S_exitButton.getX()+S_exitButton.getWidth()) && (touchY>=S_exitButton.getY()) && touchY<=(S_exitButton.getY()+S_exitButton.getHeight()) ){
+                Gdx.app.exit(); // выход из приложения
+            }
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        startButton.dispose();
+        exitButton.dispose();
+        sBatch.dispose();
+        Cyber.dispose();
+    }
+}
